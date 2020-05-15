7e00704d9b6b878ef9dbb4bc9c6b5ac0aea738d0
Caleb Seeling <primocaleb@gmail.com>
2020-05-15 19:07:12 +0200

    worked on point system

--- app/src/main/java/app/playstore/uClimb/MVP/MVP_Workout/Model_Workout.java
+++
@@ -1,4 +0,0 @@
-package app.playstore.uClimb.MVP.MVP_Workout;
-
-public class Model_Workout {
-}
--- app/src/main/java/app/playstore/uClimb/MVP/MVP_Workout/Presenter_Workout.java
+++
@@ -1,7 +0,0 @@
-package app.playstore.uClimb.MVP.MVP_Workout;
-
-public class Presenter_Workout {
-    public void getDataWorkout() {
-
-    }
-}
