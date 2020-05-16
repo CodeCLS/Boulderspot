bf5addf3e32a5c91f6390d9fe3329a601dcc5698
Caleb Seeling <primocaleb@gmail.com>
2020-05-16 19:22:34 +0200

    finishing work
    Signed-off-by: Caleb <primocaleb@gmail.com>

--- app/src/main/java/app/playstore/uClimb/MVP/MVP_Custom_Post/Model_Custom_Post.java
+++
@@ -1,4 +0,0 @@
-package app.playstore.uClimb.MVP.MVP_Custom_Post;
-
-public class Model_Custom_Post {
-}
--- app/src/main/java/app/playstore/uClimb/MVP/MVP_Location/Model_Location.java
+++
@@ -1,4 +0,0 @@
-package app.playstore.uClimb.MVP.MVP_Location;
-
-public class Model_Location {
-}
